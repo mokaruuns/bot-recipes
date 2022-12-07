f577e957f69ae575dd22950682added110573876
Marsel Akhmetov <marsel.ahmetov974@gmail.com>
2022-12-07 13:35:44 +0300

    added config file

---
+++ config.php
@@ -0,0 +1,8 @@
+<?php
+const GROUP_SECRET = 'aNj7a8nsGgs7Hu';
+
+const API_CONFIRMATION_TOKEN = 'f016dd11';
+
+const GROUP_ID = 217187964;
+
+const BOT_TOKEN = "vk1.a.msKTN7RmT8wl4BZLT4BcEcp-VUuhxP527NbEC1e2f6a-VDRqoZPHtK62fmLFeLv2n6aBq7G6sCQo5MUYkaiwnXLBi4oEbX7Gxi2soHibUV9QHylantU9Lbm-hq_AvdnrZEcKMJx1Dwq6G0u72hvS1xh8NZcv-olG_68iaRYtjE0p4VgUMrHbS3xTlfrPJb0_hsavbmNtlwAv94nwRGfQeA";
