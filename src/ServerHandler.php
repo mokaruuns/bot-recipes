96bc99ce49fe6c5e4ead0053bdee6f677ccbb213
Marsel Akhmetov <marsel.ahmetov974@gmail.com>
2022-12-07 14:52:48 +0300

    trying 2

--- src/ServerHandler.php
+++ src/ServerHandler.php
@@ -1,6 +1,6 @@
 <?php
 
-require_once realpath(dirname(__FILE__)) . "/../../config.php";
+require_once realpath(dirname(__FILE__)) . "/../config.php";
 
 
 use VK\CallbackApi\Server\VKCallbackApiServerHandler;
